b995be3429cfc2b3d826e3ff63e9fa17a97f5beb
Mina Sami <mina.s.saad@pharma.asu.edu.eg>
2026-07-14 15:23:11 +0300

    feat(forms): install tolerant URL handling platform-wide

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -2,6 +2,9 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import "./mobile-platform.css";
+import { installTolerantUrlInputs } from "./lib/url-inputs";
+
+installTolerantUrlInputs();
 
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
